83424eca38e061d4e7356e0e90a3547e5069e322
yuchao.sun <yuchao.sun@ly.com>
2024-12-13 15:50:43 +0800

    ceshi

---
+++ feign-mock-server2/src/test/kotlin/com/example/feignmockserver2/FeignMockServer2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.feignmockserver2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FeignMockServer2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
