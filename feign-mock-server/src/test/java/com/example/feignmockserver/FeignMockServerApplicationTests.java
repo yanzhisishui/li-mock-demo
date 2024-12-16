b9474efa6bd90c98f7d97ca0aa26caba67aba545
1481232419@qq.com <1481232419@qq.com>
2024-12-16 20:22:40 +0800

    init

---
+++ feign-mock-server/src/test/java/com/example/feignmockserver/FeignMockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feignmockserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignMockServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
